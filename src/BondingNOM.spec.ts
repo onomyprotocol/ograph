70b801d94d5feb5bdf3193be8db880851c4923b4
dzmitryhil <dzmitry.hil@connectwise.com>
2021-08-16 20:54:07 +0300

    WNOMHistoricalFrame entity (with core logic), unit test configs and test sample

---
+++ src/BondingNOM.spec.ts
@@ -0,0 +1,53 @@
+import {computePrice, Frame, FrameType} from './BondingNOM';
+
+import {
+    Transaction
+} from "../generated/BondingNOM/BondingNOM"
+
+import { WNOMTransaction } from "../generated/schema"
+import {BigInt} from "@graphprotocol/graph-ts";
+
+jest.mock('@graphprotocol/graph-ts', () => {
+    return {
+        BigInt: class BigInt {
+            static fromI32: (value: number) => number = (value) => value
+        },
+    };
+});
+
+jest.mock('../generated/BondingNOM/BondingNOM', () => {
+    return {
+        Transaction: class Transaction {
+            constructor() {
+                this.transaction = {
+                    from: {
+                        toHex: jest.fn()
+                    }
+                }
+            }
+            transaction: any
+        },
+    };
+});
+
+jest.mock('../generated/schema', () => {
+    return {
+        WNOMTransaction: class WNOMTransaction {
+            static load: any = (...args: any) => jest.fn()(...args)
+            save: any = jest.fn()
+        },
+    };
+});
+
+describe.only.each([
+    // type, block timestamp, start timestamp, end timestamp
+    [FrameType.Minute, 1628085048, 1628085000, 1628085059, "Minute-1628085000"],
+])('new Frame', (type, timestamp, startTime, endTime, id) => {
+    test(`type ${type}`, () => {
+        let frame = new Frame(timestamp, type)
+        expect(frame.type).toBe(type)
+        expect(frame.startTime).toBe(startTime)
+        expect(frame.endTime).toBe(endTime)
+        expect(frame.getID()).toBe(id)
+    });
+});
--- src/BondingNOM.ts
+++ src/BondingNOM.ts
@@ -1,11 +1,56 @@
-import {Transaction} from "../generated/BondingNOM/BondingNOM"
-import {WNOMTransaction} from "../generated/schema";
+import {Transaction as WNOMTransactionEvent} from "../generated/BondingNOM/BondingNOM"
+import {WNOMHistoricalFrame, WNOMTransaction} from "../generated/schema";
+import {BigInt} from "@graphprotocol/graph-ts";
 
-export function handleBondingNOMTransactionEvent(event: Transaction): void {
-    updateWNOMTransactionEntity(event);
+// TODO build class with mappings instead
+export enum FrameType {
+    Minute
 }
 
-function updateWNOMTransactionEntity(event: Transaction): void {
+// Hint to let FrameType be string and be compilable at the same time
+export namespace FrameType {
+    export function toString(type: FrameType): string {
+        switch (type) {
+            case FrameType.Minute:
+                return "Minute"
+            default:
+                throw new Error(`Unexpected WNOMHistoricalFrameType ${type}`);
+        }
+    }
+}
+
+export class Frame {
+    type: FrameType;
+    startTime: i32
+    endTime: i32
+
+    constructor(timestamp: i32, type: FrameType) {
+        let scale = 0
+        switch (type) {
+            case FrameType.Minute:
+                scale = 60
+                break;
+            default:
+                throw new Error(`Unexpected WNOMHistoricalFrameType ${type}`);
+        }
+
+        this.type = type
+        let roundedTimestamp = (Math.floor(timestamp / scale)) * scale as i32
+        this.startTime = roundedTimestamp
+        this.endTime = roundedTimestamp + scale - 1
+    }
+
+    getID(): string {
+        return join([FrameType.toString(this.type), this.startTime.toString()])
+    }
+}
+
+export function handleBondingNOMTransactionEvent(event: WNOMTransactionEvent): void {
+    updateWNOMTransactionEntity(event);
+    updateWNOMHistoricalFrame(event);
+}
+
+export function updateWNOMTransactionEntity(event: WNOMTransactionEvent): void {
     let timeStamp = event.block.timestamp;
     // ID: ${msg.sender}-${timeStamp}-${buy/sell}
     let id = join([event.params._by.toHexString(), timeStamp.toString(), event.params.buyOrSell.toString()]);
@@ -26,6 +71,50 @@ function updateWNOMTransactionEntity(event: Transaction): void {
     trx.slippage = event.params.slippage;
 
     trx.save();
+}
+
+export function updateWNOMHistoricalFrame(event: WNOMTransactionEvent): void {
+    let timeStamp = event.block.timestamp;
+
+    let frameType = FrameType.Minute
+    let frame = new Frame(timeStamp.toI32(), frameType)
+
+    // ID: ${type}-${startTime}
+    let id = frame.getID();
+
+    let historicalFrame = WNOMHistoricalFrame.load(id)
+    if (historicalFrame == null) {
+        historicalFrame = new WNOMHistoricalFrame(id);
+        historicalFrame.type = FrameType.toString(frameType);
+        historicalFrame.updateTime = timeStamp
+
+        // TODO handle if start is first second, then start price is not prev price it is current supply based price
+        historicalFrame.startTime = BigInt.fromI32(frame.startTime)
+        historicalFrame.startPrice = computePrevPrice(event.params.buyOrSell, event.params.amountNOM, event.params.supply)
+
+        historicalFrame.endTime = BigInt.fromI32(frame.endTime)
+        historicalFrame.endPrice = computePrice(event.params.supply);
+
+        historicalFrame.save();
+        return
+    }
+
+    // TODO check that current update time is grater then in the event
+    historicalFrame.updateTime = timeStamp
+    historicalFrame.endPrice = computePrice(event.params.supply);
+
+    historicalFrame.save();
+}
+
+function computePrevPrice(buyOrSell: string, amountNOM: BigInt, supply: BigInt): BigInt {
+    if (buyOrSell === "buy") {
+        return computePrice(supply.minus(amountNOM))
+    }
+    return computePrice(supply.plus(amountNOM))
+}
+
+export function computePrice(supply: BigInt): BigInt {
+    return supply.div(BigInt.fromI32(100_000_000)).pow(2)
 }
 
 function join(args: Array<string>): string {
